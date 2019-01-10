f5abfb0987ad92bb6af283b2609f24f36bd255e9
singhjason96 <jason.singh@baruchmail.cuny.edu>
2019-01-10 15:20:42 -0500

    removed invalid reference

--- db/migrate/20181223165510_create_courses.rb
+++ db/migrate/20181223165510_create_courses.rb
@@ -4,7 +4,7 @@ class CreateCourses < ActiveRecord::Migration[5.2]
       t.string :name
       t.time :start_time
       t.time :end_time
-      t.references :cohort, foreign_key: true
+      t.integer :cohort_id, foreign_key: true
 
 
       t.timestamps
